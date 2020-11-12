97f7ba7c744448b9c62b69cdee4da32851d9e917
Parkduksung <duksung1234@naver.com>
2020-11-12 23:24:22 +0900

    Activate Library

---
+++ tensor/src/main/java/com/example/tensor/Tensor.kt
@@ -0,0 +1,11 @@
+package com.example.tensor
+
+import android.content.Context
+import android.widget.Toast
+
+class Tensor {
+
+    fun test(context: Context) {
+        Toast.makeText(context, "연결됨..", Toast.LENGTH_SHORT).show()
+    }
+}
